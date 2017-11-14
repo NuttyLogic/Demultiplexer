7d9ecc9e50472ac0b031e6357015c6cc597b44bf
NuttyLogic <colinpfarrell@gmail.com>
2017-11-14 09:54:10 -0800

    Working Refactored Demultiplexer - Fixed Mismatch Bug

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def my_test_suite():
     return test_suite
 
 setup(name='Demultiplexer',
-      version='0.2.0',
+      version='0.2.1',
       description='Demultiplex Illumina lane .qseq to sample .fastq',
       author='Colin Farrell',
       author_email='colinpfarrell@gmail.com',
